fd3acce9881c203d8af7eaed02038a537ca1faab
Iamneo <techadmin@iamneo.ai>
2023-08-10 16:07:28 +0000

    Backup Commit

---
+++ reactapp/src/components/ControlButtons/ControlButtons.jsx
@@ -0,0 +1,38 @@
+import React , { useRef } from "react";
+import "./ControlButtons.css";
+  
+export default function ControlButtons(props) 
+{
+  const StartButton = (
+    <div className="btn-grp">
+        <div className="btn btn-one btn-start"
+            onClick={props.handleStart} data-testid="start">
+        Start
+        </div>
+        <div className="btn btn-two" disabled data-testid="reset">
+        Reset
+        </div>
+    </div>
+  );
+  const ActiveButtons = (
+    <div className="btn-grp">
+      
+        {props.isPaused ? <div className="btn btn-two" onClick={props.handleReset} data-testid="resume">
+        Resume
+      </div> : <div className="btn btn-two" onClick={props.handleReset} data-testid="pause">
+        Pause
+      </div>}
+      
+      <div className="btn btn-two" onClick={props.handleReset} data-testid="reset">
+        Reset
+      </div>
+      
+    </div>
+  );
+  
+  return (
+    <div className="Control-Buttons">
+      <div>{props.active ? ActiveButtons : StartButton}</div>
+    </div>
+  );
+}
